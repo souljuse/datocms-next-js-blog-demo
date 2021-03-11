e2e014e3fc467810b1f9cf600d230f657125edc0
Irene Oppo <i.oppo@datocms.com>
2021-03-11 10:20:56 +0100

    join

--- components/cta.js
+++ components/cta.js
@@ -1,18 +1,14 @@
 export default function Cta() {
   return (
     <section>
-      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
-        <div>
-          <h4 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <p>Leave your email to join the waitlist</p>
-          </h4>
-        </div>
-        <div>
-          <form className="search">
-            <input type="text" />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
+      <div className="half">
+        <h4 className="half__title">
+          <p>Leave your email to join the waitlist</p>
+        </h4>
+        <form className="eform">
+          <input type="text" />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </section>
   );
