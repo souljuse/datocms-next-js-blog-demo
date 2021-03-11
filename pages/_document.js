acf92103124c17cff7b564f4b4dc0ab2d4d17b79
Irene Oppo <i.oppo@datocms.com>
2021-03-11 14:15:36 +0100

    tracking

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,32 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="oE8V6u4A7Rzt-otqsotcAuHKZt8j1e4GFs813swO6JQ"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+          dataLayer.push(arguments);
+        }
+        gtag("js", new Date());
+
+        gtag("config", "G-V584YEBDJP");`
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
