b3f2b052b787bb0f3dc62fcb9f3fe5db2fcea4b0
Irene Oppo <i.oppo@datocms.com>
2021-03-11 14:47:25 +0100

    fix

--- pages/_document.js
+++ pages/_document.js
@@ -10,15 +10,17 @@ export default class MyDocument extends Document {
             content="oE8V6u4A7Rzt-otqsotcAuHKZt8j1e4GFs813swO6JQ"
           />
           <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-V584YEBDJP"
+          ></script>
+          <script
             dangerouslySetInnerHTML={{
               __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-        gtag("js", new Date());
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        gtag("config", "G-V584YEBDJP");`
+  gtag('config', 'G-V584YEBDJP');`
             }}
           />
         </Head>
